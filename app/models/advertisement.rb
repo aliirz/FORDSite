525d9b691e3adf8b16f9b622583c9c271d4b0261
Muhammad Sayyam <mhd.sayyam@yahoo.com>
2012-05-24 23:41:32 +0500

    updated

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,6 +1,5 @@
 class Advertisement < ActiveRecord::Base
 	has_attached_file :pic, 
-	:styles => { :medium => "300x300>", :thumb => "100x100>" },
 	:url  => "/assets/advertisements/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/advertisements/:id/:style/:basename.:extension"
 end
